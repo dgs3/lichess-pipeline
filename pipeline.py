8c959ea5c865dce9548bf4476df082fa2762e3cf
Dave Sayles <dsayles@neverware.com>
2020-05-02 19:14:11 -0400

    Put the lichess token in conducto secrets.

--- pipeline.py
+++ pipeline.py
@@ -8,46 +8,50 @@ import conducto
 GAMES_DATA_FILE = 'games.json'
 PLAYER_NAME = 'dgs3'
 
+PIPELINE_IMG = conducto.Image("python:3.7", copy_dir=".")
 
-def env_vars() -> conducto.Exec:
-    """Executor to get some env vars to supply to the conducto pipeline.
 
-    Mostly used to contain the lichess API token.
-    """
-    cmd = 'env | grep -e LICHESS_TOKEN'
-    return conducto.Exec(cmd, doc=conducto.util.magic_doc(), name='env-vars')
+def get_lichess_token() -> str:
+    """Get the lichess token from my account's secrets."""
+    token = conducto.api.Auth().get_token_from_shell()
+    api = conducto.api.Secrets()
+    secrets = api.get_user_secrets(token)
+    return secrets['LICHESS_TOKEN']
 
 
 def download_games() -> conducto.Exec:
     """Executor to Download lichess games."""
+    token = get_lichess_token()
     args = [
-        'python3.7',
+        'venv/bin/python3.7',
         'scripts/download_lichess_data.py',
         f'--player-name={PLAYER_NAME}',
         f'--output-file={GAMES_DATA_FILE}',
+        f'--token={token}',
     ]
     cmd = ' '.join(args)
-    return conducto.Exec(cmd, name='download-from-lichess')
+    return conducto.Exec(cmd)
 
 
 def openings_win_loss_rate() -> conducto.Exec:
     """Executor to analyze openings from lichess data."""
     args = [
-        'python3.7',
+        'venv/bin/python3.7',
         'scripts/openings_win_rate.py',
         f'--input-file={GAMES_DATA_FILE}',
         f'--player-name={PLAYER_NAME}',
     ]
     cmd = ' '.join(args)
-    return conducto.Exec(cmd, name='openings_win_loss_rate')
+    return conducto.Exec(cmd)
 
 
 def lichess() -> conducto.Exec:
-    """A data pipeline to pull all of my lichess games and see what openings
-    I need to work on, if I equalize out of openings, etc.
+    """Executor for the lichess pipeline.
+
+    This pipeline pulls all of my lichess games and sees how I can improve.
     """
-    with conducto.Serial(doc=conducto.util.magic_doc()) as pipeline:
-        pipeline['env-vars'] = env_vars()
+    with conducto.Serial(doc=conducto.util.magic_doc(),
+                         image=PIPELINE_IMG) as pipeline:
         pipeline['download-from-lichess'] = download_games()
         with conducto.Parallel(name='analyze-games') as analyze_pipeline:
             analyze_pipeline[
